5e7ef6457d273fb130f972aeaceb174c8fe86278
Lake Brant <lake.brant@du.edu>
2021-01-20 18:01:26 -0700

    created labels and grouped labels

--- bonus.js
+++ bonus.js
@@ -87,5 +87,102 @@ function renderCircles(circlesGroup, newXScale, newYScale, chosenXAxis, chosenYA
 }
 
 // function used for updating circles group with new tooltip
-functtion updateToolTip(chosen)
+function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
+    var label;
 
+    if (chosenXAxis === 'poverty') {
+        label = 'poverty'
+    }
+    else if (chosenXAxis === 'age') {
+        label = 'p'
+    }
+    else {
+        label = 'income'
+    }
+
+    var toolTip = d3.tip()
+        .attr('class', 'tooltip')
+        .offest([80, -60])
+        .html(function (d) {
+            return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
+        });
+    
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on('mouseover', function(data) {
+        toolTip.show(data);
+    })
+        .on('mouseout', function(data, index) {
+            toolTip.hide(data);
+        });
+
+    return circlesGroup;
+}
+
+d3.csv('data.csv').then(function(data, err) {
+    if (err) throw err;
+
+    // parse data
+    data.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.age = +data.age;
+        data.income = +data.income;
+    });
+
+    // linear scale
+    var xLinearScale = xScale(data, chosenXAxis);
+
+    // create y scale function
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.poverty)])
+        .range([height, o]);
+    
+    // initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    var xAxis = chartGroup.append('g')
+        .classed('x-axis', true)
+        .attr('transform', `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append('g')
+        .call(leftAxis);
+    
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xLinearScale(d[chosenXAxis]))
+        .attr('cy', d => yLinearScale(d[chosenYAxis]))
+        .attr('fill', 'pink')
+        .attr('opacity', '.5');
+    
+    // create group for x-axis labels
+    var XlabelsGroup = chartGroup.append('g')
+        .attr('transform', `translate(${width / 2}, ${height + 20})`);
+    
+    var povertyLabel = XlabelsGroup.append('text')
+        .attr('x', 0)
+        .attr('y', 20)
+        .attr('value', 'poverty')
+        .classed('active', true)
+        .text('In Poverty (%)');
+    
+    var ageLabel = XlabelsGroup.append('text')
+        .attr('x', 0)
+        .attr('y', 20)
+        .attr('value', 'age')
+        .classed('active', true)
+        .text('Age (Median)');
+        
+    var incomeLabel = XlabelsGroup.append('text')
+        .attr('x', 0)
+        .attr('y', 20)
+        .attr('value', 'income')
+        .classed('active', true)
+        .text('Household Income (Median)');
+})
